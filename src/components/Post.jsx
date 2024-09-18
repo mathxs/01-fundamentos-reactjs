8f75d4290330023a9b54e01e2416364c2cb1e4d5
mathxs <matheusdsantosp@gmail.com>
2024-09-18 16:41:29 -0300

    aula 2

---
+++ src/components/Post.jsx
@@ -0,0 +1,31 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/mathxs.png" />
+          <div className={styles.authorInfo}>
+            <strong>Matheus</strong>
+            <span>Dev</span>
+          </div>
+        </div>
+        <time title='18 de setembro de 2024 as 15:10' dateTime='2024-09-18 15:10:00'>
+          Publicado há 1 hora
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 {' '}<a href=''>jane.design/doctorcare</a></p>
+        <p>
+          <a href=''> #novoprojeto</a>{' '}
+          <a href=''> #nlw </a>{' '}
+          <a href=''> #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from 'phosphor-react'
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar} >
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1510218830377-2e994ea9087d?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/mathxs.png"/>
+
+        <strong>Matheus</strong>
+        <span>Dev</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
